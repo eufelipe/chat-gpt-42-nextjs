b712f6ecb79dbb5a40543fe860d1af8b5f6a6242
Felipe Rosas <felipe.rosas@g.globo>
2023-04-15 20:27:16 -0300

    chore: update messages items

--- src/app/components/Messages.tsx
+++ src/app/components/Messages.tsx
@@ -1,17 +1,38 @@
+import Image from "next/image";
+
 export default function Messages() {
   return (
-    <div className="bg-white shadow-md rounded p-6">
-      <div className="overflow-y-auto h-80 mb-6 border-b border-gray-300 pb-4">
-        <div className="mb-4">
-          <div className="text-blue-500 font-semibold">ChatGPT42:</div>
-          <div className="bg-blue-100 text-blue-800 rounded p-2 inline-block">
-            Olá! Como posso ajudar?
+    <div className="overflow-y-auto h-80 mb-6">
+      <div className="pb-2">
+        <div className="p-6 max-w-3xl mx-auto">
+          <div className="flex items-center space-x-4">
+            <Image
+              src="/icon.png"
+              alt="Picture of the author"
+              className="w-10 h-10 rounded-full"
+              width={32}
+              height={32}
+            />
+
+            <div className="text-gray-100">Olá! Como posso ajudar?</div>
           </div>
         </div>
-        <div className="mb-4">
-          <div className="text-green-500 font-semibold">Usuário:</div>
-          <div className="bg-green-100 text-green-800 rounded p-2 inline-block">
-            Preciso de informações sobre o universo.
+      </div>
+
+      <div className="bg-gray-700 pb-2">
+        <div className="p-6 max-w-3xl mx-auto bg-gray-700">
+          <div className="flex items-center space-x-4">
+            <Image
+              src="https://github.com/eufelipe.png"
+              alt="Picture of the author"
+              className="w-10 h-10 rounded-full"
+              width={32}
+              height={32}
+            />
+
+            <div className="text-gray-100">
+              Preciso de informações sobre o universo.
+            </div>
           </div>
         </div>
       </div>
