83c991cca99f90610d0ac76d316ed95a47668695
Felipe Rosas <felipe.rosas@g.globo>
2023-04-15 21:31:45 -0300

    chore: add Mensages components

---
+++ src/components/Messages.tsx
@@ -0,0 +1,16 @@
+import { IMessage } from "@/models";
+import Message from "./Message";
+
+type MessageProps = {
+  data: IMessage[];
+};
+
+export default function Messages({ data }: MessageProps) {
+  return (
+    <div className="overflow-y-auto h-80 mb-6">
+      {data.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </div>
+  );
+}
