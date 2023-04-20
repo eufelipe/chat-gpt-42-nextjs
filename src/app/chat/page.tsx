42909a09f0217677c8261eb51da58c6b455677d1
Felipe Rosas <contato@eufelipe.com>
2023-04-19 21:12:36 -0300

    chore: update chat screen

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { ChatForm, Messages, Sidebar } from "@/components";
+import { useChat } from "@/contexts/ChatContext";
+
+export default async function Chat() {
+  const { messages } = useChat();
+  const router = useRouter();
+
+  const { status } = useSession({
+    required: true,
+  });
+
+  useEffect(() => {
+    if (!status || !router) return;
+
+    if (status !== "authenticated") {
+      router.push("/signin");
+    }
+  }, [router, status]);
+
+  return (
+    <div className="h-screen flex">
+      <Sidebar />
+      <div className="flex flex-col w-full">
+        <div className="flex-1 overflow-y-auto">
+          <Messages data={messages} />
+        </div>
+
+        <div className="bg-gray-700 h-48">
+          <ChatForm />
+        </div>
+      </div>
+    </div>
+  );
+}
