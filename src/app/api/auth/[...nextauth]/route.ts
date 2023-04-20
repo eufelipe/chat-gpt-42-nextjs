25976d3230d51bd3f27cd3d97c65e0be36d59176
Felipe Rosas <contato@eufelipe.com>
2023-04-19 21:10:10 -0300

    chore: add nextauth config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const GOOGLE_CLIENT_ID = String(process.env.GOOGLE_CLIENT_ID);
+const GOOGLE_CLIENT_SECRET = String(process.env.GOOGLE_CLIENT_SECRET);
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  theme: {
+    colorScheme: "dark",
+  },
+  callbacks: {
+    async signIn({ user }) {
+      let isAllowedToSignIn = true;
+      const allowedUser = ["contato@eufelipe.com"];
+      console.log(user);
+      if (allowedUser.includes(String(user.email))) {
+        isAllowedToSignIn = true;
+      } else {
+        isAllowedToSignIn = false;
+      }
+      return isAllowedToSignIn;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
