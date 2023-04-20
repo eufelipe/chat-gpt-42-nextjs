a6ab377df5129d1ecff230b0ad05538466f4b654
Felipe Rosas <contato@eufelipe.com>
2023-04-19 22:12:43 -0300

    chore: update sidbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,11 @@
+import { signOut } from "next-auth/react";
+
 export default function Sidebar() {
-  return <div className="bg-zinc-900 w-72"></div>;
+  return (
+    <div className="bg-zinc-900 w-72">
+      <div className="flex flex-col justify-end h-full p-10  h-30">
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+    </div>
+  );
 }
