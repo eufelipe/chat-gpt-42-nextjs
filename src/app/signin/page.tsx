2b212f9c71380b5166d79b938d06101504415e97
Felipe Rosas <contato@eufelipe.com>
2023-04-19 21:12:49 -0300

    chore: add sign in screen

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-import { Cachalote, Petunia } from "@/components";
+import { Cachalote, Cloud, Petunia } from "@/components";
 import Image from "next/image";
 
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (!status || !router) return;
+
+    if (status === "authenticated") {
+      router.push("/chat");
+    }
+  }, [router, status]);
+
   return (
     <div className="h-screen w-screen">
+      <Cloud />
+
       <div className="flex justify-center items-center h-screen">
         <Cachalote />
         <Petunia />
@@ -27,7 +44,10 @@ export default function Home() {
           Log in with your Google account to continue
         </p>
 
-        <button className="bg-teal-900 text-white px-4 py-2 rounded-full mx-auto block mt-4">
+        <button
+          onClick={() => signIn()}
+          className="bg-teal-900 text-white px-4 py-2 rounded-full mx-auto block mt-4"
+        >
           Entrar com o Google
         </button>
       </div>
