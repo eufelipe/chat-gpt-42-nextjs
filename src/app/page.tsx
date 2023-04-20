199c2c2c814812a03dfe87eac8434caee2469042
Felipe Rosas <contato@eufelipe.com>
2023-04-19 21:13:06 -0300

    chore: update home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import { ChatForm, Messages, Sidebar } from "@/components";
-import { useChat } from "@/contexts/ChatContext";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { messages } = useChat();
+  const router = useRouter();
+
+  const { status } = useSession({
+    required: true,
+  });
+
+  useEffect(() => {
+    if (!status || !router) return;
+
+    const isAuthenticated = status === "authenticated";
+    router.push(isAuthenticated ? "/chat" : "/signin");
+  }, [router, status]);
 
   return (
     <div className="h-screen flex">
-      <Sidebar />
-      <div className="flex flex-col w-full">
-        <div className="flex-1 overflow-y-auto">
-          <Messages data={messages} />
-        </div>
-
-        <div className="bg-gray-700 h-48">
-          <ChatForm />
-        </div>
-      </div>
+      <p>loading...</p>
     </div>
   );
 }
